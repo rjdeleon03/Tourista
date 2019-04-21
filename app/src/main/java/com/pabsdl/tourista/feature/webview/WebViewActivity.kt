4b770e585bb9b4f3d32cb2fe81378db6d3a6f596
Pabs <pabs@alink-group.co.jp>
2019-04-22 07:28:38 +0800

    Add options in web view

--- app/src/main/java/com/pabsdl/tourista/feature/webview/WebViewActivity.kt
+++ app/src/main/java/com/pabsdl/tourista/feature/webview/WebViewActivity.kt
@@ -1,8 +1,16 @@
 package com.pabsdl.tourista.feature.webview
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.pabsdl.tourista.R
@@ -20,16 +28,22 @@ class WebViewActivity : AppCompatActivity() {
 
         setSupportActionBar(webToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Loading..."
-        supportActionBar?.subtitle = url
+        supportActionBar?.title = ""
+        supportActionBar?.subtitle = ""
 
         webView.settings.javaScriptEnabled = true
         webView.loadUrl(url)
         webView.webViewClient = object: WebViewClient() {
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                supportActionBar?.title = "Loading..."
+                supportActionBar?.subtitle = webView.url
+            }
+
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
                 supportActionBar?.title = view?.title
-                supportActionBar?.subtitle = webView.url
             }
         }
     }
@@ -47,5 +61,33 @@ class WebViewActivity : AppCompatActivity() {
             webView.goBack()
         else
             finish()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_web_view, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item == null) return true
+
+        when(item.itemId) {
+            R.id.menuWebBookmark -> {
+                /* TODO: Save as bookmark */
+            }
+            R.id.menuWebCopyUrl -> {
+                /* Copy URL to clipboard */
+                val cm = (getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager)
+                cm.primaryClip = ClipData.newPlainText("text", webView.url)
+            }
+            else -> {
+                /* Open in external browser */
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(webView.url))
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
