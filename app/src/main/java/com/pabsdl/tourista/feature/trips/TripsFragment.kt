f6138f8b6f811f0cbbf9d00355f775cb99ac533c
Pabs <pabs@alink-group.co.jp>
2019-04-30 07:51:32 +0800

    Display test map successfully in trips fragment

--- app/src/main/java/com/pabsdl/tourista/feature/trips/TripsFragment.kt
+++ app/src/main/java/com/pabsdl/tourista/feature/trips/TripsFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.here.android.mpa.common.MapSettings
+import com.here.android.mpa.common.OnEngineInitListener
+import com.here.android.mpa.mapping.SupportMapFragment
 
 import com.pabsdl.tourista.R
+import java.io.File
 
 class TripsFragment : Fragment() {
 
@@ -28,6 +32,17 @@ class TripsFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(TripsViewModel::class.java)
         // TODO: Use the ViewModel
+
+        val externalFilesDir = context!!.applicationContext.getExternalFilesDir(null)
+        val success = MapSettings.setIsolatedDiskCacheRootPath("$externalFilesDir${File.separator}.here-maps", resources.getString(R.string.app_map_intent))
+
+        val mapFragment = (childFragmentManager.findFragmentById(R.id.tripsMapFragment) as SupportMapFragment)
+        mapFragment.init { error ->
+            if (error == OnEngineInitListener.Error.NONE) {
+                val map = mapFragment.map
+                val x = 5
+            }
+        }
     }
 
 }
